ca0b263c970852e5ab380f122c850d4efe42b04b
Asia <asiaellis5@gmail.com>
2020-05-27 14:13:07 +0100

    set up jest and super test

--- server/index.js
+++ server/index.js
@@ -1,9 +1,22 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const pool = require("./db");
 
 app.use(cors());
 app.use(express.json());
+
+// ROUTES //
+
+// create a tweet
+
+// get tweets
+
+// get a single tweet
+
+// update a tweet
+
+// delete a tweet
 
 app.listen(5000, () => {
   console.log("server has started on port 5000");
