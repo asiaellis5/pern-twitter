eab90d110f07533fd36d29b6cb9e9f1367e4745f
Asia <asiaellis5@gmail.com>
2020-05-28 19:52:35 +0100

    added a edit tweet component

---
+++ client/src/components/EditTweet/editTweet.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const EditTweet = () => {
+  return (
+    <div className="all-tweets-container">
+      <h1>Edit Tweet</h1>
+    </div>
+  );
+};
+
+export default EditTweet;
