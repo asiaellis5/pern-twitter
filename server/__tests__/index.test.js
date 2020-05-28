45b6c41faaa0810912a8574e97a176430f6b25ff
Asia <asiaellis5@gmail.com>
2020-05-28 12:53:17 +0100

    added route to select a certain tweet

--- server/__tests__/index.test.js
+++ server/__tests__/index.test.js
@@ -3,11 +3,9 @@ const supertest = require("supertest");
 const request = supertest(app);
 const pool = require("./../test_db.js");
 
-let tweets;
-
-beforeAll(() => {
-  process.env.NODE_ENV = "test";
-});
+// beforeAll(() => {
+//   process.env.NODE_ENV = "test";
+// });
 
 afterAll(() => {
   pool.query("TRUNCATE TABLE tweets;");
@@ -22,7 +20,6 @@ it("posts to /tweets", async (done) => {
       expect(response.statusCode).toBe(200);
       expect(response.body[0].description).toBe("test tweet");
     });
-
   done();
 });
 
@@ -34,21 +31,37 @@ it("gets from /tweets", async (done) => {
       expect(response.statusCode).toBe(200);
       expect(response.body.length).toEqual(1);
     });
-
   done();
 });
 
-// it("updates a tweet", async (done) => {
-//   const tweets = await pool.query("SELECT * FROM tweets");
-//   const id = tweets.rows[0].tweet_id;
-//   const res = await request
-//     .put(`/tweets/${id}`)
-//     .send({ description: "new test tweet" })
+it("gets a single tweet from /tweets", async (done) => {
+  const addTweet = await request
+    .post("/tweets")
+    .send({ description: "Another test tweet" });
+  const tweets = await request.get("/tweets").then((response) => {
+    return response.body[1].tweet_id;
+  });
+  const res = await request
+    .get(`/tweets/${tweets}`)
 
-//     .then((response) => {
-//       expect(response.statusCode).toBe(200);
-//       expect(response.body[0].description).toBe("new test tweet");
-//     });
+    .then((response) => {
+      expect(response.statusCode).toBe(200);
+      expect(response.body.description).toBe("Another test tweet");
+    });
+  done();
+});
 
-//   done();
-// });
+it("updates a from /tweets/:id", async (done) => {
+  const tweets = await request.get("/tweets").then((response) => {
+    return response.body[0].tweet_id;
+  });
+  const res = await request
+    .put(`/tweets/${tweets}`)
+    .send({ description: "new test tweet" })
+
+    .then((response) => {
+      expect(response.statusCode).toBe(200);
+      expect(response.body).toEqual("Tweet was updated");
+    });
+  done();
+});
--- server/index.js
+++ server/index.js
@@ -26,8 +26,8 @@ app.post("/tweets", async (req, res) => {
     );
 
     res.json(newTweet.rows);
-  } catch (error) {
-    console.error(error.message);
+  } catch (err) {
+    console.error(err.message);
   }
 });
 
@@ -38,8 +38,8 @@ app.get("/tweets", async (req, res) => {
     const allTweets = await pool.query("SELECT * FROM tweets;");
 
     res.json(allTweets.rows);
-  } catch (error) {
-    console.error(error.message);
+  } catch (err) {
+    console.error(err.message);
   }
 });
 
@@ -48,13 +48,13 @@ app.get("/tweets", async (req, res) => {
 app.get("/tweets/:id", async (req, res) => {
   try {
     const { id } = req.params;
-    const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [
+    const tweet = await pool.query("SELECT * FROM tweets WHERE tweet_id = $1", [
       id,
     ]);
 
-    res.json(todo.rows[0]);
-  } catch (error) {
-    console.error(error.messsage);
+    res.json(tweet.rows[0]);
+  } catch (err) {
+    console.error(err.messsage);
   }
 });
 
@@ -65,8 +65,8 @@ app.put("/tweets/:id", async (req, res) => {
     const { id } = req.params;
     const { description } = req.body;
 
-    const updateTodo = await pool.query(
-      "UPDATE todo SET description = $1 WHERE todo_id = $2",
+    const updateTweet = await pool.query(
+      "UPDATE tweets SET description = $1 WHERE tweet_id = $2",
       [description, id]
     );
 
