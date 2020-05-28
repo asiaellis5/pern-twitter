95ca56defc6b1289badc409914b2487de6d17d57
Asia <asiaellis5@gmail.com>
2020-05-28 16:56:44 +0100

    added snapshot tests for app and login page

--- client/src/components/LogInPage/loginPage.test.js
+++ client/src/components/LogInPage/loginPage.test.js
@@ -2,9 +2,14 @@ import React from "react";
 import { shallow } from "enzyme";
 import LogInPage from "./loginPage";
 
-describe("App", () => {
+describe("LogInPage", () => {
+  let wrapper;
+
+  beforeEach(() => (wrapper = shallow(<LogInPage />)));
+
+  it("should render correctly", () => expect(wrapper).toMatchSnapshot());
+
   it("should render a <div />", () => {
-    const wrapper = shallow(<LogInPage />);
     expect(wrapper.find("div").length).toEqual(1);
   });
 });
