e626c3733042def36d451bdae3e5ccbfb77df05c
Asia <asiaellis5@gmail.com>
2020-05-28 17:03:43 +0100

    added a log in form and button

--- client/src/components/LogInPage/loginPage.js
+++ client/src/components/LogInPage/loginPage.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 const LogInPage = () => {
-  return <div className="login-container">Log In</div>;
+  return (
+    <div className="login-container">
+      <form className="d-flex mt-5">
+        <button className="btn btn-success">Log in</button>
+      </form>
+    </div>
+  );
 };
 
 export default LogInPage;
--- client/src/components/LogInPage/loginPage.test.js
+++ client/src/components/LogInPage/loginPage.test.js
@@ -12,4 +12,8 @@ describe("LogInPage", () => {
   it("should render a <div />", () => {
     expect(wrapper.find("div").length).toEqual(1);
   });
+
+  it("should render a <form />", () => {
+    expect(wrapper.find("form").length).toEqual(1);
+  });
 });
