6ff88701e01ca5b4c546d0dfb53f028c751723ce
Asia <asiaellis5@gmail.com>
2020-05-28 17:33:11 +0100

    added a heading and test to addTweet

--- client/src/components/AddTweet/addTweet.js
+++ client/src/components/AddTweet/addTweet.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const AddTweet = () => {
-  return <div className="addTweet-Container"></div>;
+  return (
+    <div className="addTweet-Container">
+      <h1>Add Tweet</h1>
+    </div>
+  );
 };
 
 export default AddTweet;
--- client/src/components/AddTweet/addTweet.test.js
+++ client/src/components/AddTweet/addTweet.test.js
@@ -10,4 +10,8 @@ describe("AddTweet", () => {
   it("should render a <div />", () => {
     expect(wrapper.find("div").length).toEqual(1);
   });
+
+  it("should render a <h1 />", () => {
+    expect(wrapper.find("h1").length).toEqual(1);
+  });
 });
