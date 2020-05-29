088c15d8ea3e0f09a33a46321195785c91c7cc1d
Asia <asiaellis5@gmail.com>
2020-05-29 12:42:16 +0100

    added state description and sets it when type in form

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import LogInPage from "./components/LogInPage/loginPage";
+import HomePage from "./components/HomePage/homePage";
 
 function App() {
   return (
     <div className="app-container">
       <LogInPage />
+      <HomePage />
     </div>
   );
 }
--- client/src/components/AddTweet/addTweet.js
+++ client/src/components/AddTweet/addTweet.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 const AddTweet = () => {
+  const [description, setDescription] = useState("");
   return (
     <div className="addTweet-Container">
       <h1>Add Tweet</h1>
       <form>
-        <input type="text" className="form-control" value={"Add Tweet"} />
+        <input
+          type="text"
+          id="add-tweet"
+          className="form-control"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
         <button className="btn btn-success">Tweet</button>
       </form>
     </div>
