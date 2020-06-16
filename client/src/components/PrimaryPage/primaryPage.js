ebeac5776d642b1e382a5417a7ef5ffcd52f2958
Asia <asiaellis5@gmail.com>
2020-06-16 17:43:28 +0100

    added some basic styling to the primary page

--- client/src/components/PrimaryPage/primaryPage.js
+++ client/src/components/PrimaryPage/primaryPage.js
@@ -1,14 +1,16 @@
 import React, { useImperativeHandle } from "react";
 import { Link } from "react-router-dom";
+import classes from "./primaryPage.module.css";
 
 const PrimaryPage = () => {
   return (
-    <div>
+    <div className={classes.primaryContainer}>
+      <h1>Welcome to Chitter</h1>
       <Link to="/login">
-        <button className="btn btn-success">Log in</button>
+        <button className="btn btn-success m-3">Log in</button>
       </Link>
       <Link to="/signup">
-        <button className="btn btn-success">Sign Up</button>
+        <button className="btn btn-success m-3">Sign Up</button>
       </Link>
     </div>
   );
