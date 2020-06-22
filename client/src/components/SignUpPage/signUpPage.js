4fae008196936208adaa7959ff55ef253c902e1a
Asia <asiaellis5@gmail.com>
2020-06-22 15:57:38 +0100

    added sign up

--- client/src/components/SignUpPage/signUpPage.js
+++ client/src/components/SignUpPage/signUpPage.js
@@ -1,13 +1,52 @@
-import React, { useImperativeHandle } from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const SignUpPage = () => {
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onSubmitForm = async (event) => {
+    event.preventDefault();
+
+    const body = { email: email, username: username, password: password };
+    const response = await fetch("http://localhost:5000/users", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(body),
+    });
+    window.location = "/home";
+  };
+
+
   return (
     <div className="login-container">
       <h1 className="text-center mt-5">Sign Up</h1>
       <form className="d-flex mt-5">
-        <input type="text" className="form-control" value={"email"} />
-        <input type="text" className="form-control" value={"password"} />
+        <input
+          className="form-control"
+          type="text"
+          required
+          placeholder="Enter a Username"
+          value={username}
+          onChange={(event) => setUsername(event.target.value)}
+        />
+        <input
+          className="form-control"
+          type="email"
+          required
+          placeholder="Enter a Valid Email Address"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
+        <input
+          className="form-control"
+          type="password"
+          required
+          placeholder="Enter a Secure Password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
         <Link to="/home">
           <button className="btn btn-success">Sign up</button>
         </Link>
