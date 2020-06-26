546d80aac29dcfec7901abbb7e7bb10de620aaab
Asia <asiaellis5@gmail.com>
2020-06-26 10:30:40 +0100

    added test for getting users

--- server/__tests__/index.test.js
+++ server/__tests__/index.test.js
@@ -3,96 +3,119 @@ const supertest = require("supertest");
 const request = supertest(app);
 const pool = require("./../test_db.js");
 
-// beforeAll(() => {
-//   pool.query(
-//     "INSERT INTO Users (email,username,password) VALUES ('email@example.com', 'test', 'password')"
-//   );
-// });
 
-afterAll(() => {
-  pool.query("TRUNCATE TABLE tweets;");
-});
 
-it("posts to /tweets/1", async (done) => {
+describe("posts", () => {
 
-  const res = await request
-    .post("/tweets/1")
-    .send({ description: "test tweet" })
+  // beforeAll(() => {
+  //   pool.query(
+  //     "INSERT INTO Users (user_id, email,username,password) VALUES (1, 'email@example.com', 'test', 'password')"
+  //   );
+  // });
 
-    .then((response) => {
-      expect(response.statusCode).toBe(200);
-      expect(response.body[0].description).toBe("test tweet");
+  afterAll(() => {
+    pool.query("TRUNCATE TABLE tweets;");
+  });
+
+  it("posts to /tweets/1", async (done) => {
+
+    const res = await request
+      .post("/tweets/1")
+      .send({ description: "test tweet" })
+
+      .then((response) => {
+        expect(response.statusCode).toBe(200);
+        expect(response.body[0].description).toBe("test tweet");
+      });
+    done();
+  });
+
+  it("gets from /tweets", async (done) => {
+    const res = await request
+      .get("/tweets")
+
+      .then((response) => {
+        expect(response.statusCode).toBe(200);
+        expect(response.body.length).toEqual(1);
+      });
+    done();
+  });
+
+  it("gets a single tweet from /tweets", async (done) => {
+    const addTweet = await request
+      .post("/tweets/1")
+      .send({ description: "Another test tweet" });
+    const tweets = await request.get("/tweets").then((response) => {
+      return response.body[1].tweet_id;
     });
-  done();
-});
+    const res = await request
+      .get(`/tweets/${tweets}`)
 
-it("gets from /tweets", async (done) => {
-  const res = await request
-    .get("/tweets")
+      .then((response) => {
+        expect(response.statusCode).toBe(200);
+        expect(response.body.description).toBe("Another test tweet");
+      });
+    done();
+  });
 
-    .then((response) => {
-      expect(response.statusCode).toBe(200);
+  it("updates a from /tweets/:id", async (done) => {
+    const tweets = await request.get("/tweets").then((response) => {
+      return response.body[0].tweet_id;
+    });
+    const res = await request
+      .put(`/tweets/${tweets}`)
+      .send({ description: "new test tweet" })
+
+      .then((response) => {
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toEqual("Tweet was updated");
+      });
+
+    const updated = await request.get("/tweets").then((response) => {
+      expect(response.body[1].description).toEqual("new test tweet");
+    });
+    done();
+  });
+
+  it("deletes a tweets from /tweets/:id", async (done) => {
+    const tweets = await request.get("/tweets").then((response) => {
+      return response.body[0].tweet_id;
+    });
+    const res = await request
+      .delete(`/tweets/${tweets}`)
+
+      .then((response) => {
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toEqual("Tweet was deleted");
+      });
+
+    const deleted = await request.get("/tweets").then((response) => {
+      console.log(response.body);
       expect(response.body.length).toEqual(1);
+      expect(response.body).not.toContain({
+        tweet_id: `${tweets}`,
+        description: "test tweet",
+      });
     });
-  done();
-});
-
-it("gets a single tweet from /tweets", async (done) => {
-  const addTweet = await request
-    .post("/tweets/1")
-    .send({ description: "Another test tweet" });
-  const tweets = await request.get("/tweets").then((response) => {
-    return response.body[1].tweet_id;
+    done();
   });
-  const res = await request
-    .get(`/tweets/${tweets}`)
 
-    .then((response) => {
-      expect(response.statusCode).toBe(200);
-      expect(response.body.description).toBe("Another test tweet");
-    });
-  done();
-});
+})
 
-it("updates a from /tweets/:id", async (done) => {
-  const tweets = await request.get("/tweets").then((response) => {
-    return response.body[0].tweet_id;
+describe("users", () => {
+  afterAll(() => {
+    pool.query("DELETE FROM Users WHERE userid > 0;");
   });
-  const res = await request
-    .put(`/tweets/${tweets}`)
-    .send({ description: "new test tweet" })
 
-    .then((response) => {
-      expect(response.statusCode).toBe(200);
-      expect(response.body).toEqual("Tweet was updated");
-    });
+  it("gets the users", async (done) => {
+    const res = await request
+      .get("/users")
 
-  const updated = await request.get("/tweets").then((response) => {
-    expect(response.body[1].description).toEqual("new test tweet");
-  });
-  done();
-});
-
-it("deletes a tweets from /tweets/:id", async (done) => {
-  const tweets = await request.get("/tweets").then((response) => {
-    return response.body[0].tweet_id;
-  });
-  const res = await request
-    .delete(`/tweets/${tweets}`)
-
-    .then((response) => {
-      expect(response.statusCode).toBe(200);
-      expect(response.body).toEqual("Tweet was deleted");
-    });
-
-  const deleted = await request.get("/tweets").then((response) => {
-    console.log(response.body);
-    expect(response.body.length).toEqual(1);
-    expect(response.body).not.toContain({
-      tweet_id: `${tweets}`,
-      description: "test tweet",
-    });
-  });
-  done();
-});
+      .then((response) => {
+        expect(response.statusCode).toBe(200);
+        expect(response.body.length).toEqual(1);
+      });
+    done();
+  })
+})
 
